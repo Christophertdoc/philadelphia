c3aa1bb770332087850a07877ee22d852d38256f
Christopher Dougherty <christophertdoc@gmail>
2020-02-12 17:46:44 -0500

    views

--- southPhilly/urls.py
+++ southPhilly/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /southPhilly/
     path('', views.index, name='index'),
+    # ex: /southPhilly/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /southPhilly/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /southPhilly/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
