785ba5d077c581561850caaeb746aff0f641bee0
Christopher Dougherty <christophertdoc@gmail>
2020-02-17 09:16:27 -0500

    tutorial 3 to tutorial 7

--- southPhilly/urls.py
+++ southPhilly/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'southPhilly'
 urlpatterns = [
-    # ex: /southPhilly/
-    path('', views.index, name='index'),
-    # ex: /southPhilly/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /southPhilly/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /southPhilly/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
